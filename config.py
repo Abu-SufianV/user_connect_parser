d6a3c731617cc75f80c79a7c78f5279a3be15525
Dr Proger <dr-proger@a-vagabov.ru>
2023-03-15 12:33:21 +0300

    Added logging

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
+# Настройки логирования
+import logging
+from datetime import datetime
+
+log_filename = f"./logs/{datetime.date(datetime.now())}.log".replace("-", "_")
+log_format = "%(asctime)s [%(levelname)s]: %(message)s"
+log_level = logging.INFO
+log_encoding = "UTF-8"
+logging.basicConfig(level=log_level, filename=log_filename, format=log_format, encoding=log_encoding)
+
 # Путь до лога и название
 LOG_PATH         = "./"
 LOG_NAME_PATTERN = "ObjectSpawner.txt"
@@ -7,4 +17,4 @@ CSV_PREFIX = "user_parser__"
 CSV_NAME   = "result.csv"
 
 # Относительный путь до промежуточных файлов
-INTERMEDIATE_PATH = "./intermediate_csv/"
+INTERMEDIATE_PATH = "./intermediate_csv"
--- user_connect_parser.py
+++ user_connect_parser.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import re
+import logging
 from datetime import datetime as dttm
 from config import *
 
@@ -13,11 +14,11 @@ def find_need_log(path: str, pattern: str) -> str:
     :param pattern: Шаблон названия файла
     :return: Название файла
     """
-    files = os.listdir(LOG_PATH)
+    files = os.listdir(path)
     pattern = dttm.strftime(dttm.now(), pattern)
     for file in files:
         if re.search(pattern, file):
-            print(file)
+            return file
 
 
 def obj_spaw_parser(path: str, log_name: str) -> dict:
@@ -28,7 +29,8 @@ def obj_spaw_parser(path: str, log_name: str) -> dict:
     :param log_name: Название лога, который будет парситься
     :return: Словарь с последними авторизациями пользователей {user:last_connect}
     """
-    with open(path + log_name, 'r') as obj_spaw_log:
+    full_path = str(path) + str(log_name)
+    with open(full_path, 'r') as obj_spaw_log:
 
         connections = {}
         rows = obj_spaw_log.readlines()
@@ -57,20 +59,22 @@ def dict_to_dataframe(dict_users: dict) -> pd.DataFrame:
     return pd.DataFrame([[k, v] for k, v in dict_users.items()], columns=['Users', 'Last connect time'])
 
 
-def dataframe_to_csv(df: pd.DataFrame, file_name: str, tmp=False) -> None:
+def dataframe_to_csv(df: pd.DataFrame, file_name: str, tmp=False) -> str:
     """
     Функция выгрузки DataFrame в *.csv
 
     :param file_name: Название выходного файла
     :param df: DataFrame
     :param tmp: Флаг промежуточного файла
+    :return: Имя выгруженного файла
     """
     if tmp:
-        result_name = f'intermediate_csv/{file_name}{dttm.strftime(dttm.now(), "%y_%m_%d__%H_%M")}.csv'
+        result_name = f'{INTERMEDIATE_PATH}/{file_name}{dttm.strftime(dttm.now(), "%y_%m_%d__%H_%M")}.csv'
     else:
         result_name = file_name
     df.to_csv(result_name, header=True, index=False, sep=';',
               encoding='utf-8', date_format='%Y-%m-%d %H:%M:%S')
+    return result_name
 
 
 def list_files_csv(path: str, prefix: str) -> list:
@@ -81,7 +85,7 @@ def list_files_csv(path: str, prefix: str) -> list:
     :param prefix: Префикс файлов
     :return: Список путей до файлов
     """
-    files = os.listdir(INTERMEDIATE_PATH)
+    files = os.listdir(path)
     files_csv = []
     for file in files:
         if prefix == file[:13] and file[-3:] == 'csv':
@@ -89,14 +93,14 @@ def list_files_csv(path: str, prefix: str) -> list:
     return files_csv
 
 
-def join_dataframes(files: list[str]) -> pd.DataFrame:
+def join_dataframes(files: list) -> pd.DataFrame:
     """
     Функция объединения нескольких csv-файлов в один DataFrame
 
     :param files: Список путей до csv-файлов
     :return: DataFrame с объединёнными данными
     """
-    return pd.concat([pd.read_csv(f'{INTERMEDIATE_PATH}{file}', sep=';') for file in files], ignore_index=True)
+    return pd.concat([pd.read_csv(f'{INTERMEDIATE_PATH}/{file}', sep=';') for file in files], ignore_index=True)
 
 
 def uniq_data(df: pd.DataFrame) -> pd.DataFrame:
@@ -110,18 +114,49 @@ def uniq_data(df: pd.DataFrame) -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-    # Ищем нужный файл
-    file_name = find_need_log(path=LOG_PATH, pattern=LOG_NAME_PATTERN)
+    logging.info("Процесс анализа ObjectSpawner — запущен")
+    try:
+        logging.info("Поиск нужного файла")
+        obj_spw_log = find_need_log(path=LOG_PATH, pattern=LOG_NAME_PATTERN)
+        logging.info(f"Файл: {obj_spw_log}")
+    except Exception as er:
+        logging.error(f"Ошибка при поиске нужного файла: {er}")
+        raise "Ошибка при поиске нужного файла"
 
-    # Собираем данные с лога
-    log_data = obj_spaw_parser(path=LOG_PATH, log_name=file_name)
-    data = dict_to_dataframe(dict_users=log_data)
 
-    # Выгружаем промежуточные csv-файлы
-    dataframe_to_csv(df=data, file_name=CSV_PREFIX, tmp=True)
+    try:
+        logging.info("Собираем данные с ObjectSpawner")
+        log_data = obj_spaw_parser(path=LOG_PATH, log_name=obj_spw_log)
+        data = dict_to_dataframe(dict_users=log_data)
+        logging.info(f"Полученные данные: \n{data}")
+    except Exception as er:
+        logging.error(f"Ошибка при сборе данных: {er}")
+        raise f"Ошибка при сборе данных: {er}"
+    try:
 
-    # Унифицируем данные
-    data = uniq_data(df=join_dataframes(files=list_files_csv(path='', prefix=CSV_PREFIX)))
+        logging.info("Выгружаем промежуточный csv-файл")
+        tmp_file = dataframe_to_csv(df=data, file_name=CSV_PREFIX, tmp=True)
+        logging.info(f"Данные выгружены корректно в файл '{tmp_file}'")
+    except Exception as er:
+        logging.error(f"Ошибка при выгрузке в файл: {er}")
+        raise f"Ошибка при выгрузке в файл: {er}"
+
+    try:
+        logging.info("Унифицируем данные из всех csv-файлов")
+        data = uniq_data(df=join_dataframes(files=list_files_csv(path=INTERMEDIATE_PATH, prefix=CSV_PREFIX)))
+        logging.info("Данные уникальны")
+    except Exception as er:
+        logging.error(f"Ошибка при унификации данных: {er}")
+        raise f"Ошибка при унификации данных: {er}"
+
 
     # Выгружаем итоговый csv-файл
-    dataframe_to_csv(df=data, file_name=CSV_NAME)
+    try:
+        logging.info(f"Выгружаем {CSV_NAME}")
+        dataframe_to_csv(df=data, file_name=CSV_NAME)
+        logging.info(f"Файл {CSV_NAME} выгружен успешно")
+    except Exception as er:
+        logging.error(f"Ошибка при выгрузке {CSV_NAME}: {er}")
+        raise f"Ошибка при выгрузке {CSV_NAME}: {er}"
+
+    logging.info("Процесс анализа ObjectSpawner — окончен")
